e155e76718b481454e6a32d42d65398bf0d6dd08
Andre <jcsteele26@gmail.com>
2020-08-19 09:12:01 -0400

    Code for Question Class

---
+++ Quiz.java
@@ -0,0 +1,13 @@
+public class Quiz{
+
+    public static void main(String[] args) {
+        
+        String q1 = "What color are apples?\n" + "(a)red\n(b)orange\n(c)magenta\n";
+
+        String q2 = "What color are bananas?\n" + "(red)\n(blue)\n(yellow)\n";
+
+        
+    }
+
+
+}
